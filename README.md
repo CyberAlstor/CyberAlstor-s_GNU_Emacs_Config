05d39dc4c265dadb2f32313ff0d7263cd7f914c6
CyberAlstor <ArchAlstor21@gmail.com>
2026-04-26 12:44:19 -0600

    Color theme selected
    I picked a color theme for my config. I choose Solarized Selenized Dark for my theme.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# CyberAlstor-s_GNU_Emacs_Config
+# CyberAlstor's GNU Emacs Config
+
 My personal GNU Emacs config.
+
+# Features
+
+Default Theme: Solarized Selenized Dark (https://emacsthemes.com/themes/solarized-selenized-dark)
+
+## Working autocomplete (C/C++).
+
+NOTE: Python and JS mode-hooks exists, but are untested.
+
+- LSP server : Eglot
+  - Current hooks : C/C++, Python, JS
+- Orderless + Cape (extends autocomplete features)
+- Autocomplete UI : Corfu
+  - Use TAB key to use corfu-next (next autocomplete option)
--- init.el
+++ init.el
@@ -42,7 +42,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(cape corfu orderless)))
+ '(custom-enabled-themes '(solarized-selenized-dark))
+ '(custom-safe-themes
+   '("d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36"
+     "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633"
+     "efcecf09905ff85a7c80025551c657299a4d18c5fcfedd3b2f2b6287e4edd659"
+     "57a29645c35ae5ce1660d5987d3da5869b048477a7801ce7ab57bfb25ce12d3e"
+     default))
+ '(package-selected-packages '(cape corfu orderless solarized-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

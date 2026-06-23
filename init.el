126e89a7eabcc7174ad8633de54e4ab54d17ced5
CyberAlstor <CyberAlstor@protonmail.com>
2026-06-23 12:53:32 -0600

    added Evil Mode to Emacs configuration

--- init.el
+++ init.el
@@ -14,7 +14,10 @@
 (require 'magit)
 (require 'neotree)
 (require 'vterm)
+(require 'evil)
 
+;; Enable Evil Mode
+(evil-mode 1)
 
 ;; MELPA
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -82,7 +85,8 @@
    '("833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633"
      default))
  '(package-selected-packages
-   '(cape corfu enlight magit neotree orderless solarized-theme vterm)))
+   '(cape corfu enlight evil magit neotree orderless solarized-theme
+	  vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

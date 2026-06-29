592c277017f13d4daab5717ca00897c3d1d31875
CyberAlstor <CyberAlstor@protonmail.com>
2026-06-29 12:57:46 -0600

    Changed theme to solarized-light

--- init.el
+++ init.el
@@ -17,9 +17,9 @@
    '("4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d"
      default))
  '(package-selected-packages
-   '(company diff-hl doom-themes flycheck nerd-icons treemacs-icons-dired
-	     treemacs-magit treemacs-persp treemacs-projectile
-	     treemacs-tab-bar vterm)))
+   '(company diff-hl doom-themes flycheck nerd-icons solarized-theme
+	     treemacs-icons-dired treemacs-magit treemacs-persp
+	     treemacs-projectile treemacs-tab-bar vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- package-configs.el
+++ package-configs.el
@@ -1,25 +1,29 @@
-;; Doom-themes
-(use-package doom-themes
-  :ensure t
-  :custom
-  ;; Global settings (defaults)
-  (doom-themes-enable-bold t)   ; if nil, bold is universally disabled
-  (doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  ;; for treemacs users
-  (doom-themes-treemacs-theme "doom-tokyo-night") ; use "doom-colors" for less minimal icon theme
-  :config
-  (load-theme 'doom-tokyo-night t)
+;; THEME
+(load-theme 'solarized-light t)
 
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (nerd-icons must be installed!)
-  ;;(doom-themes-neotree-config)
-  ;; or for treemacs users
-  (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+;; Doom-themes
+;; (use-package doom-themes
+;;   :ensure t
+;;   :custom
+;;   ;; Global settings (defaults)
+;;   (doom-themes-enable-bold t)   ; if nil, bold is universally disabled
+;;   (doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;;   ;; for treemacs users
+;;   (doom-themes-treemacs-theme "doom-tokyo-night") ; use "doom-colors" for less minimal icon theme
+;;   :config
+;;   (load-theme 'doom-tokyo-night t)
+
+;;   ;; Enable flashing mode-line on errors
+;;   (doom-themes-visual-bell-config)
+;;   ;; Enable custom neotree theme (nerd-icons must be installed!)
+;;   ;;(doom-themes-neotree-config)
+;;   ;; or for treemacs users
+;;   (doom-themes-treemacs-config)
+;;   ;; Corrects (and improves) org-mode's native fontification.
+;;   (doom-themes-org-config))
 ;; End of Doom-themes
 
+;; END OF THEME
 
 ;; Org Mode
 (global-set-key (kbd "C-c l") #'org-store-link)
